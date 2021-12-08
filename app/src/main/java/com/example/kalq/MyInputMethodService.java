497240890f348184fb2acfa4eccd61d84f6be051
ivolarevic <46626092+ivolarevic@users.noreply.github.com>
2021-12-08 15:31:21 +0100

    Long press on delete key

--- app/src/main/java/com/example/kalq/MyInputMethodService.java
+++ app/src/main/java/com/example/kalq/MyInputMethodService.java
@@ -1,16 +1,25 @@
 package com.example.kalq;
 
+import android.annotation.SuppressLint;
 import android.inputmethodservice.InputMethodService;
+import android.text.TextUtils;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
 import android.widget.Button;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class MyInputMethodService extends InputMethodService implements View.OnClickListener {
+import java.util.Timer;
+
+@SuppressLint("ClickableViewAccessibility")
+public class MyInputMethodService extends InputMethodService {
 
     private ConstraintLayout kalqLayout;
+    public static boolean flag = false;
+    Button delete;
+    Timer timer = new Timer();
 
     @Override
     public void onInitializeInterface() {
@@ -20,26 +29,70 @@ public class MyInputMethodService extends InputMethodService implements View.OnC
     @Override
     public View onCreateInputView() {
         kalqLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.kalq_keyboard_view, null);
+        Button delete = kalqLayout.findViewById(R.id.del);
+
+        delete.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                InputConnection ic = getCurrentInputConnection();
+                long time;
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        flag = true;
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                long time = 800;
+                                while (flag) {
+
+                                    CharSequence selectedText = ic.getSelectedText(0);
+                                    if (TextUtils.isEmpty(selectedText)) {
+                                        ic.deleteSurroundingText(1, 0);
+                                    } else {
+                                        ic.commitText("", 1);
+                                    }
+
+                                    if (time > 50){ time -= 100;
+                                    } else { time = 50; }
+
+                                    try {
+                                        Thread.sleep(time);
+
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        }).start();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        flag = false;
+                        break;
+                }
+
+                return false;
+            }
+        });
 
         return kalqLayout;
     }
 
     public void space(View v){
+        flag = false;
         InputConnection ic = getCurrentInputConnection();
         ic.commitText(" ",1);
     }
 
     public void enter(View v) {
+        flag = false;
         InputConnection ic = getCurrentInputConnection();
         ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
     }
 
-    public void delete(View v){
-        InputConnection ic = getCurrentInputConnection();
-        ic.deleteSurroundingText(1, 0);
-    }
 
     public void letter(View v){
+        flag = false;
         Button b = v.findViewById(v.getId());
         CharSequence text = b.getText();
 
@@ -47,8 +100,4 @@ public class MyInputMethodService extends InputMethodService implements View.OnC
         ic.commitText(text.toString(), 1);
     }
 
-    @Override
-    public void onClick(View v) {
-
-    }
 }
